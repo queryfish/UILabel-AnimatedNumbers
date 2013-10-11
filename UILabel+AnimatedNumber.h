567989e5a1f6520afc49f43c52bbb311ef4d9ad5
Steve <marrowsky@126.com>
2013-10-12 03:21:10 +0800

    WOrking ONE

---
+++ UILabel+AnimatedNumber.h
@@ -0,0 +1,13 @@
+//
+//  UILabel+AnimatedNumber.h
+//  AnimatedLabel
+//
+//  Created by Steve on 13-10-7.
+//  Copyright (c) 2013年 Steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel_AnimatedNumber : UILabel
+
+@end
---
+++ UILabel+AnimatedNumber.m
@@ -0,0 +1,31 @@
+//
+//  UILabel+AnimatedNumber.m
+//  AnimatedLabel
+//
+//  Created by Steve on 13-10-7.
+//  Copyright (c) 2013年 Steve. All rights reserved.
+//
+
+#import "UILabel+AnimatedNumber.h"
+
+@implementation UILabel_AnimatedNumber
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
